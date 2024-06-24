e6604763368effaef6938578dca61ee94e7af8c9
S. David Kyle <sdavidkyle@gmail.com>
2024-06-23 18:17:03 -0700

    Add project files.

---
+++ PublisherData/PubContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PublisherDomain;
+
+namespace PublisherData;
+
+public class PubContext : DbContext
+{
+    public DbSet<Author> Authors { get; set; }
+    public DbSet<Book> Books { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Data Source=davaputer;Initial Catalog=PubDatabase;User ID=sa;Password=Alpha155;Trust Server Certificate=True");
+    }
+}
