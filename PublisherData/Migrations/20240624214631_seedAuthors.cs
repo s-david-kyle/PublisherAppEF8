17ea701869a070266d3f0e635685c0524be4a73d
S. David Kyle <sdavidkyle@gmail.com>
2024-06-24 17:43:08 -0700

    Adding migrations and practicing with OnModeCreating

---
+++ PublisherData/Migrations/20240624214631_seedAuthors.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace PublisherData.Migrations
+{
+    /// <inheritdoc />
+    public partial class seedAuthors : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Authors",
+                columns: new[] { "Id", "FirstName", "LastName" },
+                values: new object[,]
+                {
+                    { 1, "Josie", "Newf" },
+                    { 2, "Jill", "Hill" },
+                    { 3, "Jack", "Black" },
+                    { 4, "Jen", "Ken" },
+                    { 5, "Jennifer", "Kensington" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "BookId", "AuthorId", "BasePrice", "PublishDate", "Title" },
+                values: new object[,]
+                {
+                    { 1, 1, 19.99m, new DateOnly(2021, 10, 12), "The Book of Josie" },
+                    { 2, 1, 29.99m, new DateOnly(2022, 10, 12), "The Book of Josie 2" },
+                    { 3, 2, 19.99m, new DateOnly(2021, 10, 12), "The Book of Jill" },
+                    { 4, 2, 29.99m, new DateOnly(2022, 10, 12), "The Book of Jill 2" },
+                    { 5, 3, 19.99m, new DateOnly(2021, 10, 12), "The Book of Jack" },
+                    { 6, 3, 29.99m, new DateOnly(2022, 10, 12), "The Book of Jack 2" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "BookId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "BookId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "BookId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "BookId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "BookId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "BookId",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
--- PublisherDomain/Author.cs
+++ PublisherDomain/Author.cs
@@ -2,7 +2,7 @@
 
 public class Author
 {
-    public int Id { get; set; }
+    public int AuthorId { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public List<Book> Books
